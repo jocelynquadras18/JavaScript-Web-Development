c867877dcaddd805eb8cefab3a44fa083c7b97c1
Jocelyn Quadras <jocelynquadras18@gmail.com>
2018-05-31 18:24:57 -0400

    If else

---
+++ Scripts/scripts.js
@@ -0,0 +1,14 @@
+
+ var name='John';
+ var isMarried=false
+ //If else
+ //== does type conversion
+ //=== doesnt do type conversion
+ if(isMarried === 0)
+ {
+   console.log('True');
+ }
+ else
+{
+console.log('False');
+ }
