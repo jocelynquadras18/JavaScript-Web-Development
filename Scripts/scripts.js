4ebab2c54aa1e18f123f970df43c834b64dfe6e6
Jocelyn Quadras <jocelynquadras18@gmail.com>
2018-05-31 18:46:58 -0400

    Boolean Logic and Switch statement

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,14 +1,39 @@
 
- var name='John';
- var isMarried=false
- //If else
- //== does type conversion
- //=== doesnt do type conversion
- if(isMarried === 0)
+ // Boolean
+ //boolean logic
+ //and true if all true
+ //or true if one is true
+ //not! reverts the exression
+
+ var age=12;
+ if(age<20)
  {
-   console.log('True');
+   console.log("John is a teenager")
+ }
+ else if(age>20 && age <30)
+ {
+   console.log("John is young man")
  }
  else
 {
-console.log('False');
- }
+console.log("John is a man")
+}
+
+
+//switch statments
+
+var job="cop"
+switch(job)
+{
+  case 'teacher':
+  console.log('John teaches kid');
+  break
+  case 'driver':
+  console.log('John drives cab');
+  break
+  case 'cop':
+  console.log('John helps fight crime');
+  break;
+  default:
+  console.log('John does something else');
+}
