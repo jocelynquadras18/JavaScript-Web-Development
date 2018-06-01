83bd3621e2aa9b85d70ca89296c3fd0eba464857
Jocelyn Quadras <jocelynquadras18@gmail.com>
2018-06-01 17:34:53 -0400

    coding challenge 2

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,16 +1,46 @@
-//Loops
+//coding challenge 2
 
-// for(var i=0;i<10;i++)
-// {
-//   console.log(i);
-// }
-
-var names=['Jocelyn','Khushwant','Naini','Jeni'];
-
-
-
-
-for (var i =names.length-1;i>=0;i--)
+var years=[1990,1998,2012,2011,1980,1290];
+var just=[];
+var temp;
+for(var i =0 ; i< years.length ; i++)
 {
-  console.log(names[i]);
+  temp=2018-years[i];
+  just.push(temp);
 }
+console.log(just)
+for (var j =0; j< just.length; j++)
+{
+  if(just[j]>=18)
+  {
+    console.log('Full age' + just[j])
+  }
+}
+
+
+function printfullage(years)
+{
+  var just=[];
+  var output=[];
+  for(var i =0 ; i< years.length ; i++)
+  {
+    temp=2018-years[i];
+    just.push(temp);
+  }
+  for (var j =0; j< just.length; j++)
+  {
+    if(just[j]>=18)
+    {
+    output.push(true);
+    }
+    else
+    {
+      output.push(false);
+    }
+  }
+return output;
+}
+
+
+var output=printfullage(years);
+console.log(output);
