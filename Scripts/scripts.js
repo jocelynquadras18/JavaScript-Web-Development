25ae4173e76072a860f393d2e128f2fbac6861e0
Jocelyn Quadras <jocelynquadras18@gmail.com>
2018-06-01 15:15:13 -0400

    .gitLoops

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,36 +1,16 @@
-//arrays
+//Loops
 
-var names=['Jocelyn','Khushwant'];
-var years= new Array(1222,1228,1990);
-var ex=['Jocelyn',1992,'Developer'];
-console.log(ex);
-ex.push('Mama'); //pushes at the end
-console.log(ex);
-ex.unshift('Miss.'); //appends at begining
-console.log(ex);
-ex.pop(); //removes last element
-console.log(ex);
-console.log(ex.indexOf('Miss.'));
+// for(var i=0;i<10;i++)
+// {
+//   console.log(i);
+// }
+
+var names=['Jocelyn','Khushwant','Naini','Jeni'];
 
 
-//objects
-
-var joc={name:'Jocelyn', age:'25', lastName:'Quadras',job:'Teacher'};
-console.log(joc.name);
 
 
-var abc=new Object();
-abc.firstName='Jocelyn';
-console.log(abc);
-
-
-//Objects and Methods
-//this: used to refer to property of that object itself
-var joc={name:'Jocelyn',
-          yearOfBirth:1992,
-          calculateAge:function()
-        {
-          this.age= 2016-this.yearOfBirth;
-        }};
-joc.calculateAge();
-console.log(joc);
+for (var i =names.length-1;i>=0;i--)
+{
+  console.log(names[i]);
+}
